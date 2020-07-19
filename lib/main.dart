5869d01acd51331247cc4044395ac1a10a1a23bb
gabriel <cristhian.viera1@gmail.com>
2020-07-18 20:55:18 -0500

    add .env file

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:kimirina_app/services/user_service.dart';
 import 'package:provider/provider.dart';
 import 'routes/router.dart' as router;
 import 'routes/routes.dart';
-void main() =>runApp(MyApp());
+Future main() async {
+  await DotEnv().load('.env');
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider(
       create: (BuildContext context) => ApiService(),
       child: MaterialApp(
-        title: 'Flutter Kimirina',
+        title: 'Kimirina',
         debugShowCheckedModeBanner: false,
         onGenerateRoute: router.generateRoute,
         initialRoute: carruselViewRoute,
